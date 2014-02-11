c3f31b6247386d28bc630994b993178317902bdd
bursaar <ben@ben.ie>
2014-02-11 14:21:39 +0000

    Added comments and tidied up code a little.

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// Front matter
+	// ============
 
 	cout << "MAZE RUNNER";
 	SkipLine(3);
@@ -31,148 +33,195 @@ int _tmain(int argc, _TCHAR* argv[])
 	SkipLine(2);
 	cout << "Please maximize your window and choose an option.";
 	SkipLine(2);
-	cout << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl;
+	cout << "0. Quit" << endl << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl << "Choice: ";
+
+
+
+	// Simple menu system
+	//===================
 
 	int choice;		// Menu choice from the start of the program.
 	cin >> choice;
 
-	// Generator code for maze object.
-	bool LevelOneCode[338] = { 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-	if (choice == 4)
-	{	
-		cout << "Type filename:" << endl;
-		string filename = "TestLevel.txt";
-		cin >> filename;
 
-		std::ifstream arrayData(filename);
-
-		int arrayIndex = 0;
-		bool LevelOneReadCode[338];
-		string line;
-
-		if (arrayData.is_open())
-		{
-			while (getline (arrayData, line) )
-			{
-				if (line == "0")
-				{
-					LevelOneReadCode[arrayIndex] = 0;
-					arrayIndex++;
-				}
-				if (line == "1")
-				{
-					LevelOneReadCode[arrayIndex] = 1;
-					arrayIndex++;
-				}
-			}
-			arrayData.close();
-
-			cMaze TestMaze(LevelOneReadCode);
-			cPathfinder SolveTestMaze;
-			SolveTestMaze.create(TestMaze);
-			SolveTestMaze.print();
-
-			cin >> arrayIndex;	// Temporary TODO remove.
-		}
-	}
-
-	if (choice == 3)
+	while (choice != 0)	// Loop checking that the choice isn't set to 0, which means exit.
 	{
-		cout << "Please choose a filename, names are case-sensitive." << endl << "Filename: ";
-		string filename;
-		cin >> filename;
-		std::fstream arrayData(filename, std::ios::app);
-
-		int arrayIndex = 0;
-
-		while (arrayIndex < 338)
-		{
-			arrayData << LevelOneCode[arrayIndex] << endl;
-			arrayIndex++;
-		}
-
-		arrayData.close();
-
-	}
-
-	if (choice == 2) {
-
-		// Create Maze
-		// =========================
-
-		// Maze object.
-		cMaze LevelOne(LevelOneCode);
-
-		// Name for Maze Object
-		char LevelOneName[25] = "Test_Game_Example_01";
-
-		SkipLine();
-
-		getchar();
-
-		// Print proper values of maze.
-		// =========================
-		LevelOne.print();
-		SkipLine();
-
-		getchar();
-
-		// Perform pathfinding pass.
-		// =========================
-
-		cPathfinder pthLevelOne;		// Pathfinder instance with nodemap. One per level. Kept separate from maze for portability.
-
-		pthLevelOne.create(LevelOne);	// Populate nodemap of the entire maze.
-
-		gridloc gl_tempLegalMoves[4];	// Array to hold the current legal moves.
-		gridloc gl_tempLookHere;		// Create location to check for legal moves *from*
-
-		gl_tempLookHere.xloc = 1;		// Assign values to x & y - in this case the node at (1, 1)
-		gl_tempLookHere.yloc = 1;
-
-		// Examine next legal moves.
-		pthLevelOne.LookAround(gl_tempLookHere, gl_tempLegalMoves);
-
-		cout << "This is the node map (should be identical).";
-
-		SkipLine(2);
-		pthLevelOne.print();
-
-		SkipLine();
-
-		getchar();
-
-		cout << "These are the moves available at ";
-
-		gl_tempLookHere.printLoc(gl_tempLookHere); // TODO find a more elegant implementation where it doesn't need to be passed itself.
-
-		cout << ":";
-
-		SkipLine();
-
-		PrintCoOrds(gl_tempLegalMoves);
-
-		SkipLine(2);
-
-	}
-	
-	if (choice == 1)
-	{
-
-		// Generator code for maze object.
+		// Default generator code for maze object - used in opetions 1 (play) and 2 (demo), and by default in 3 (write).
 		bool LevelOneCode[338] = { 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
-		// Maze object.
-		cMaze LevelOne(LevelOneCode);
+		//====================//
+		// CHOICE: READ DATA //
+		//==================//
+		if (choice == 4)
+		{
+			// UI to demo data serialisation.
+			cout << "Type level name to load. Names are case-sensitive. Type 0 or Quit to end the program." << endl << "Level name: ";
+			string filename;	// Default value.
+			cin >> filename;
+			string str_extension = ".lvl";		// File extension - invisible to user in-program.
+			if (filename == "0"					// Add ability to quit.
+				|| filename == "quit" 
+				|| filename == "Quit" 
+				|| filename == "stop"
+				|| filename == "Stop"
+				|| filename == "QUIT"
+				|| filename == "STOP") choice = 0;
+			filename.append(str_extension);		// Append file extension to level name input from user.
+			std::ifstream arrayData(filename);	
 
-		// Name for Maze Object
-		char LevelOneName[25] = "Let_Us_Play";
+			int arrayIndex = 0;					// Set basic array index to make it possible to count through the lines & array elements. Start value should be 0.
+			bool LevelReadCode[338];			// Boolean array variable used to store the read code.
+			string line;						// String object to write into when looping through the file.
 
-		getchar();
+			if (arrayData.is_open())					// Check to see that file is open
+			{
+				while (getline(arrayData, line))		// Cycle through each line, reading into the "line" string variable.
+				{
+					if (line == "0")					// If the line is a 0
+					{
+						LevelReadCode[arrayIndex] = 0;	// Change the current element in the boolean array into a zero
+						arrayIndex++;					// Increment the index
+					}
+					if (line == "1")					// If the line is a 1
+					{
+						LevelReadCode[arrayIndex] = 1;	// Change the current element in the boolean array into a one
+						arrayIndex++;					// Increment the index
+					}
+				}
+				arrayData.close();						// Cleanup - close out file.
+
+				cMaze TestMaze(LevelReadCode);	// Create maze instance from loaded data.
+				cPathfinder SolveTestMaze;		// Create pathfinder instance.
+				SolveTestMaze.create(TestMaze);	// Create nodemap in pathfinder.
+				SolveTestMaze.print();			// Print nodemap to screen.
+
+				SkipLine(3);
+
+				std::cout << "0. Quit" << endl << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl << "Choice: ";
+				cin >> choice;
+
+			}
+		}
+		//=====================//
+		// CHOICE: WRITE DATA //
+		//===================//
+		if (choice == 3)
+		{
+			cout << "Type level name to load. Names are case-sensitive." << endl << "Level name: ";
+			string filename = "TestLevel.txt";
+			cin >> filename;
+			string str_extension = ".lvl";
+			filename.append(str_extension);
+
+			std::fstream arrayData(filename, std::ios::app);
+
+			int arrayIndex = 0;
+
+			if (arrayData.is_open())
+			{
+				while (arrayIndex < 338)
+				{
+					arrayData << LevelOneCode[arrayIndex] << endl;		// Put the current value of the element at the current position in the array into the file and break to a new line.
+					arrayIndex++;										// Increment the value of the index.
+				}
+
+				arrayData.close();										// Cleanup - close out file.
+			}
+
+			cout << "0. Quit" << endl << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl << "Choice: ";
+			cin >> choice;
+
+		}
+
+		//====================//
+		// CHOICE: DEMO CODE //
+		//==================//
+		if (choice == 2) {
+
+			// Create Maze
+			// =========================
+
+			// Maze object.
+			cMaze LevelOne(LevelOneCode);
+
+			// Name for Maze Object
+			char LevelOneName[25] = "Test_Game_Example_01";
+
+			SkipLine();
+
+			getchar();
+
+			// Print proper values of maze.
+			// =========================
+			LevelOne.print();
+			SkipLine();
+
+			getchar();
+
+			// Perform pathfinding pass.
+			// =========================
+
+			cPathfinder pthLevelOne;		// Pathfinder instance with nodemap. One per level. Kept separate from maze for portability.
+
+			pthLevelOne.create(LevelOne);	// Populate nodemap of the entire maze.
+
+			gridloc gl_tempLegalMoves[4];	// Array to hold the current legal moves.
+			gridloc gl_tempLookHere;		// Create location to check for legal moves *from*
+
+			gl_tempLookHere.xloc = 1;		// Assign values to x & y - in this case the node at (1, 1)
+			gl_tempLookHere.yloc = 1;
+
+			// Examine next legal moves.
+			pthLevelOne.LookAround(gl_tempLookHere, gl_tempLegalMoves);
+
+			cout << "This is the node map (should be identical).";
+
+			SkipLine(2);
+			pthLevelOne.print();
+
+			SkipLine();
+
+			getchar();
+
+			cout << "These are the moves available at ";
+
+			gl_tempLookHere.printLoc(gl_tempLookHere); // TODO find a more elegant implementation where it doesn't need to be passed itself.
+
+			cout << ":";
+
+			SkipLine();
+
+			PrintCoOrds(gl_tempLegalMoves);
+
+			SkipLine(2);
+
+			cout << "0. Quit" << endl << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl << "Choice: ";
+			cin >> choice;
+
+		}
+
+		//====================//
+		// CHOICE: PLAY GAME //
+		//==================//
+		if (choice == 1)
+		{
+
+			// Generator code for maze object.
+			bool LevelOneCode[338] = { 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+			// Maze object.
+			cMaze LevelOne(LevelOneCode);
+
+			// Name for Maze Object
+			char LevelOneName[25] = "Let_Us_Play";
+
+			std::cout << "0. Quit" << endl << "1. Test game." << endl << "2. Demo code." << endl << "3. Write data." << endl << "4. Read data." << endl << "Choice: ";
+			cin >> choice;
+		}
 	}
 
-	cout << "Press enter to quit.";
+	std::cout << "Press enter to quit.";
 
 	SkipLine();
 
